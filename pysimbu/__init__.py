8fba46b5a6b0a46f9a54648390e788f9d7d43a44
Wade Huang <fastwade11@gmail.com>
2024-04-11 13:22:02 +0800

    🐞 fix(pysimbu/__init__.py): fix some bugs

--- pysimbu/__init__.py
+++ pysimbu/__init__.py
@@ -2,14 +2,14 @@
 Author: Wade Huang
 Date: 2024-04-10 19:05:08
 LastEditors: Wade Huang
-LastEditTime: 2024-04-11 09:53:59
+LastEditTime: 2024-04-11 13:21:10
 FilePath: /PySimBu/pysimbu/__init__.py
 Description:
 
 """
 
-from dataset import SimBuDataset
-from simulator import simulate_bulk, merge_simulation, save_simulation
+from .dataset import SimBuDataset
+from .simulator import simulate_bulk, merge_simulation, save_simulation
 
 
 __version__ = "0.1.0"
